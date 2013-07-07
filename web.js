f22459547b0592deda7b2636c03bea032bf1ae85
Sudhir Subramanian <sudhir@gmail.com>
2013-07-07 02:59:20 +0000

    Fixed require fs

--- web.js
+++ web.js
@@ -1,14 +1,14 @@
 var express = require('express');
+var fs = require ('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   // get the content from index.html
-  fs.readFileiSync('index.html', function (err, data) {
+  fs.readFileSync('index.html', function (err, data) {
     if (err) throw err;
     response.send(data.toString('utf-8'));
   });
-  //response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
